f1ccf05c47f125f1258ac91f4b5f9768f918e991
Tiago Oliveira <tiago66.oliveira@hotmail.com>
2023-11-24 14:43:13 +0000

    Criaçao de projeto limpo com spring web, devtools, jpa e mysql driver

---
+++ Eclipse_Projects_SprintBoot/Semana03_09_SpringWithDBLibros/src/main/java/com/curso/inicio/Semana0309SpringWithDbLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana0309SpringWithDbLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana0309SpringWithDbLibrosApplication.class, args);
+	}
+
+}
